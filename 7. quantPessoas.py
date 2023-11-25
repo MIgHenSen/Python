6be216a00451d9f56718a35039affded3bb4895b
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-11-24 22:59:01 -0300

    Update 7. quantPessoas.py

--- 7. quantPessoas.py
+++ 7. quantPessoas.py
@@ -1,12 +1,9 @@
 homensComMaisDe18 = 0
 mulheres = 0
-#While é uma estrutura de repetição
 while True:
     print("Para saber a quantidade de pessoas...")
     s = input("Digite o sexo da pessoa [M|F]")
-    #Variavel que recebe apenas numeros inteiros
     idade = int(input("Digite a idade da pessoa: "))
-    #Estrutura if else, para ver se é homem ou mulher
     if s.upper() == "M" and idade > 18:
         homensComMaisDe18 += 1
     if s.upper() == "F":
