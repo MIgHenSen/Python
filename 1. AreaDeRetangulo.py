cec8cc079bc25ffb9db97c0bb3922483d0abce82
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-11-24 22:53:27 -0300

    Update 1. AreaDeRetangulo.py

--- 1. AreaDeRetangulo.py
+++ 1. AreaDeRetangulo.py
@@ -1,20 +1,13 @@
-#Estrutura de repetição while, para ficar repetindo o código
 while True:
-    #Try, para verificar erros nos códigos abaixo
     try:
         print("Para calcular a área de um retângulo...")
         print("Deseja o resultado em m² ou cm²?") 
-        #Variável de tipo int, que só recebe números inteiros
         unidade = int(input("Digite 1 (m²) ou 2 (cm²)"))
 
-        #Estrutura if else, para definir qual será a unidade de medida com base em "unidade"
         if unidade == 1:
-            #Inputs de tipo float, que podem receber números decimais
             base = float(input("Digite o valor da base: "))
             altura = float(input("Digite o valor da altura: "))
             
-            #Estrutura if else, que verifica se algum dos valores é zero
-            #or("ou"), método para adicionar mais condições
             if base == 0 or altura == 0:
                 print("Nenhum dos valors pode ser zero!")
                 area = 0
@@ -24,11 +17,9 @@ while True:
             print("A área do retânhgulo é: ", round(area, 2), "m²") 
             print("")
         elif unidade == 2:
-            #Inputs de tipo float, que podem receber números decimais
             base = float(input("Digite o valor da base: "))
             altura = float(input("Digite o valor da altura: "))
 
-            #Estrutura if else, que verifica se algum dos valores é zero
             if base == 0 or altura == 0:
                 print("Nenum dos valors pode ser zero!")
                 area = 0
